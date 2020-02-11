87128f089ef9444cfd9625261c9eeec514cbd857
AnaninAN <ananinpgu@mail.ru>
2020-02-11 15:58:22 +0300

    history

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ import AddIcon from'@material-ui/icons/Add'
 
 export class Header extends Component {
   render() {
-    const { chatName } = this.props;
+    const { chatName, addChat } = this.props;
 
     return (
       <div className="header">
         <div className="header-lc">
-          <Button variant="contained" color="primary" endIcon={<AddIcon />}>Add</Button>
+          <Button onClick={addChat} variant="contained" color="primary" endIcon={<AddIcon />}>Add</Button>
         </div>
     <div className="header-pc">{chatName}</div>
       </div>
--- src/containers/HeaderContainer.jsx
+++ src/containers/HeaderContainer.jsx
@@ -1,14 +1,26 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
 
 import { Header } from 'components/Header';
+import { add } from 'actions/chats';
 
 class HeaderContainer extends PureComponent {
+  handleAddChat = () => {
+    const { AddChat, newChatId, redirect } = this.props;
+    const nameChat = prompt('Введите имя чата');
+
+    if (nameChat) {
+      AddChat({ name: nameChat, chatId: newChatId });
+      redirect(newChatId);
+    }
+  }
+
   render() {
     const { chatName } = this.props;
 
     return (
-      <Header chatName={chatName} />
+      <Header addChat={this.handleAddChat} chatName={chatName} />
     );
   }
 }
@@ -17,9 +29,19 @@ function mapStateToProps(state, ownProps) {
   const chats = state.chats.get('entries');
   const { match } = ownProps;
 
+  const lastId = state.chats.get('entries').size ? state.chats.get('entries').last().get('id') : 0;
+
   return {
     chatName: chats.getIn([match.params.id, 'name']),
+    newChatId: +lastId + 1,
   }
 }
 
-export const HeaderRedux = connect(mapStateToProps)(HeaderContainer);
+function mapDispatchToProps(dispatch) {
+  return {
+    AddChat: (chat) => dispatch(add(chat)),
+    redirect: (id) => dispatch(push(`/chats/${id}`)),
+  }
+}
+
+export const HeaderRedux = connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
